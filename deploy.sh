a1444243315cc652f68b647b52b1cfedb1b4cf8f
Kapralov Sergey <skapralov@mail.ru>
2018-06-05 01:31:31 +0300

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -
+
+# TODO: check for TAG
+if [ -z "${TAG}" ]; then
+    echo "TAG must be set"
+    exit 1
+fi
+
+git checkout $(git rev-parse HEAD)
+mvn versions:set "-DnewVersion=${TAG}"
+sed "s/0.0.0-SNAPSHOT/${TAG}/g" ./README.md > /tmp/README.md
+mv /tmp/README.md ./README.md
+mvn clean deploy -Pextras,ossrh
+git commit -am "[release] oo-tests-${TAG}"
+git tag -f ${TAG}
+
